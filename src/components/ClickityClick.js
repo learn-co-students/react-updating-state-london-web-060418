a42ac5115050223908eb2722b2a94fc72e9eecea
GCdM <gabrielcm.br@gmail.com>
2018-08-08 11:29:08 +0100

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,10 @@
-// Code ClickityClick Component Here
+import React, { Component } from 'react'
+
+class ClickityClick extends Component {
+
+  render() {
+    return <div></div>
+  }
+}
+
+export default ClickityClick
